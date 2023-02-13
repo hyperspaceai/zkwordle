27fce4c2cc07533062790f51e92f32b39500b3ee
DaveHyperspace <dave@hyperspace.us>
2023-02-13 12:54:53 +0000

    fix seed data

--- prisma/seeds/words.ts
+++ prisma/seeds/words.ts
@@ -1,12 +1,12 @@
 import { getRandomWord } from "../../src/utils/word";
 
 export const generateWords = (amount: number): { gameId: number; word: string; playDay: Date }[] => {
-  let gameStart = 1;
+  let gameStart = 0;
   const todayDate = new Date().getDate();
   const data = Array.from({ length: amount }).map(() => {
     const date = new Date().setDate(todayDate + gameStart);
     const wordData = {
-      gameId: gameStart,
+      gameId: gameStart + 1,
       word: getRandomWord(),
       playDay: new Date(date),
     };
