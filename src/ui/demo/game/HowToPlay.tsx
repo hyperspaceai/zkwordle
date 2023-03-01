50af03955926b46870dd3f8b5546eafb0157b306
DaveHyperspace <dave@hyperspace.us>
2023-03-01 13:36:54 +0000

    Fix: update info modal styling for mobile view

--- src/ui/demo/game/HowToPlay.tsx
+++ src/ui/demo/game/HowToPlay.tsx
@@ -28,10 +28,10 @@ const HowToPlay = () => {
 
   useEffect(() => {
     if (router.pathname !== "/") return;
-    const notFirstVisit = JSON.parse(localStorage.getItem("notFirstVisit") || "null");
+    const notFirstVisit = JSON.parse(localStorage.getItem("notFirstVisit") || "null") as boolean | null;
     localStorage.setItem("notFirstVisit", JSON.stringify(true));
     if (!notFirstVisit) btnRef.current?.click();
-  }, []);
+  }, [router.pathname]);
 
   return (
     <>
@@ -39,63 +39,88 @@ const HowToPlay = () => {
         <Icon as={FaLightbulb} boxSize={{ base: "6", md: "8" }} />
       </Button>
 
-      <Modal isCentered isOpen={isOpen} motionPreset="slideInBottom" onClose={onClose} size={["sm", "md", "lg"]}>
+      <Modal isCentered isOpen={isOpen} motionPreset="slideInBottom" onClose={onClose} size={["xs", "md", "lg"]}>
         <ModalOverlay />
-        <ModalContent alignItems="center" textAlign="center">
-          <ModalHeader>How To Play</ModalHeader>
+        <ModalContent alignItems="center" textAlign={{ base: "left", md: "center" }}>
+          <ModalHeader pb={{ base: 0, md: 4 }} fontSize={{ base: "md", md: "xl" }}>
+            How To Play
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            <VStack alignItems="center" mb="4">
-              <Text fontSize="sm">
+          <ModalBody mt="0">
+            <VStack alignItems="center" mb={{ base: 2, md: 4 }}>
+              <Text fontSize={{ base: "xs", md: "sm" }}>
                 Guess the word in 6 tries. After each guess, the color of the tiles will change to show how close your
                 guess was to the word.
               </Text>
 
-              <Text fontSize="sm">Type the word with your Keyboard or Keypad, and press ENTER to submit.</Text>
+              <Text fontSize={{ base: "xs", md: "sm" }}>
+                Type the word with your Keyboard or Keypad, and press ENTER to submit.
+              </Text>
               <Divider />
-              <Text fontSize="md" fontWeight="semibold">
+              <Text fontSize={{ base: "sm", md: "md" }} fontWeight="semibold">
                 Examples
               </Text>
               <HStack>
-                <CharacterTile checkingGuess={false} currentIndex={7} index={0} state={LetterState.Match} value="H" />
-                <CharacterTile checkingGuess={false} currentIndex={7} index={1} state={undefined} value="E" />
-                <CharacterTile checkingGuess={false} currentIndex={7} index={2} state={undefined} value="L" />
-                <CharacterTile checkingGuess={false} currentIndex={7} index={3} state={undefined} value="L" />
-                <CharacterTile checkingGuess={false} currentIndex={7} index={4} state={undefined} value="O" />
+                <CharacterTile
+                  checkingGuess={false}
+                  currentIndex={7}
+                  index={0}
+                  proof
+                  state={LetterState.Match}
+                  value="H"
+                />
+                <CharacterTile checkingGuess={false} currentIndex={7} index={1} proof state={undefined} value="E" />
+                <CharacterTile checkingGuess={false} currentIndex={7} index={2} proof state={undefined} value="L" />
+                <CharacterTile checkingGuess={false} currentIndex={7} index={3} proof state={undefined} value="L" />
+                <CharacterTile checkingGuess={false} currentIndex={7} index={4} proof state={undefined} value="O" />
               </HStack>
-              <Text fontSize="sm">
+              <Text fontSize={{ base: "xs", md: "sm" }}>
                 The letter <b>H</b> is in the Wordle and in the correct spot.
               </Text>
               <HStack>
-                <CharacterTile checkingGuess={false} currentIndex={7} index={0} state={undefined} value="H" />
-                <CharacterTile checkingGuess={false} currentIndex={7} index={1} state={undefined} value="Y" />
-                <CharacterTile checkingGuess={false} currentIndex={7} index={2} state={undefined} value="P" />
-                <CharacterTile checkingGuess={false} currentIndex={7} index={3} state={undefined} value="E" />
-                <CharacterTile checkingGuess={false} currentIndex={7} index={4} state={LetterState.Present} value="R" />
+                <CharacterTile checkingGuess={false} currentIndex={7} index={0} proof state={undefined} value="H" />
+                <CharacterTile checkingGuess={false} currentIndex={7} index={1} proof state={undefined} value="Y" />
+                <CharacterTile checkingGuess={false} currentIndex={7} index={2} proof state={undefined} value="P" />
+                <CharacterTile checkingGuess={false} currentIndex={7} index={3} proof state={undefined} value="E" />
+                <CharacterTile
+                  checkingGuess={false}
+                  currentIndex={7}
+                  index={4}
+                  proof
+                  state={LetterState.Present}
+                  value="R"
+                />
               </HStack>
-              <Text fontSize="sm">
+              <Text fontSize={{ base: "xs", md: "sm" }}>
                 The letter <b>R</b> is in the Wordle but in the wrong spot.
               </Text>
               <HStack>
-                <CharacterTile checkingGuess={false} currentIndex={7} index={0} state={undefined} value="S" />
-                <CharacterTile checkingGuess={false} currentIndex={7} index={1} state={LetterState.Miss} value="P" />
-                <CharacterTile checkingGuess={false} currentIndex={7} index={2} state={undefined} value="A" />
-                <CharacterTile checkingGuess={false} currentIndex={7} index={3} state={undefined} value="C" />
-                <CharacterTile checkingGuess={false} currentIndex={7} index={4} state={undefined} value="E" />
+                <CharacterTile checkingGuess={false} currentIndex={7} index={0} proof state={undefined} value="S" />
+                <CharacterTile
+                  checkingGuess={false}
+                  currentIndex={7}
+                  index={1}
+                  proof
+                  state={LetterState.Miss}
+                  value="P"
+                />
+                <CharacterTile checkingGuess={false} currentIndex={7} index={2} proof state={undefined} value="A" />
+                <CharacterTile checkingGuess={false} currentIndex={7} index={3} proof state={undefined} value="C" />
+                <CharacterTile checkingGuess={false} currentIndex={7} index={4} proof state={undefined} value="E" />
               </HStack>
-              <Text fontSize="sm">
+              <Text fontSize={{ base: "xs", md: "sm" }}>
                 The letter <b>P</b> is not in the Wordle.
               </Text>
-              <Divider pt="2" />
-              <Text fontSize="xl" fontWeight="semibold">
+              <Divider pt={{ base: "1", md: "2" }} />
+              <Text fontSize={{ base: "md", md: "xl" }} fontWeight="semibold">
                 Proof Verification
               </Text>
-              <Text fontSize="sm">
+              <Text fontSize={{ base: "xs", md: "sm" }} display={{ base: "none", md: "block" }}>
                 In this context, a Zero Knowledge (ZK) proof refers to a proof that demonstrates the occurrence of a
                 specific computation. What&apos;s unique about ZK proofs is that you can verify the proof&apos;s
                 accuracy without having to re-execute the original computation.
               </Text>
-              <Text fontSize="sm">
+              <Text fontSize={{ base: "xs", md: "sm" }}>
                 Once you finish guessing the validity of your guesses and the result you got will be packaged in a
                 shareable proof using our ZKWasm runtime which can be verified by anyone in their browser. This let’s
                 other people know in a self-verifiable way whether these guesses were actually the ones used by you.
