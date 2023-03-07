009b599baf04674ada6ef5d3dada2030f11d2a8b
DaveHyperspace <dave@hyperspace.us>
2023-03-07 21:30:27 +0000

    testing social image fonts

--- src/pages/api/og/result.tsx
+++ src/pages/api/og/result.tsx
@@ -1,8 +1,8 @@
 import { ImageResponse } from "@vercel/og";
+import { metadata } from "config/metadata";
 import type { NextApiHandler, PageConfig } from "next";
 import invariant from "tiny-invariant";
 
-import { loadOptions } from "@/lib/opengraph";
 import { paramsSchema } from "@/schema/opengraph";
 import { Og } from "@/ui/opengraph";
 import { SvgHy, SvgZkwordle } from "@/ui/opengraph/svg";
@@ -11,12 +11,19 @@ export const config: PageConfig = {
   runtime: "edge",
 };
 
+const fonts = Promise.all(
+  ["/fonts/bvp-light.otf", "/fonts/bvp-regular.otf", "/fonts/bvp-bold.otf"].map((url) =>
+    fetch(new URL(url, metadata.url).toString()).then((res) => res.arrayBuffer()),
+  ),
+);
+
 const handler: NextApiHandler = async (req) => {
   invariant(typeof req.url === "string", "`req.url` is required and must be a string");
 
   const url = new URL(req.url);
   const rawParams = Object.fromEntries(url.searchParams.entries());
   const params = await paramsSchema.parseAsync(rawParams);
+  const [light, regular, bold] = await fonts;
 
   const response = new ImageResponse(
     (
@@ -34,7 +41,17 @@ const handler: NextApiHandler = async (req) => {
         <Og.LinkBanner />
       </Og.Container>
     ),
-    await loadOptions({ debug: process.env.NODE_ENV === "development" }),
+    {
+      emoji: "twemoji",
+      fonts: [
+        { name: "Be Vietnam Pro", weight: 300, data: light! },
+        { name: "Be Vietnam Pro", weight: 400, data: regular! },
+        { name: "Be Vietnam Pro", weight: 700, data: bold! },
+      ],
+      height: 630,
+      width: 1200,
+      debug: process.env.NODE_ENV === "development",
+    },
   );
 
   return response;
