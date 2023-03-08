20ee7b84e561cff521f4e08a0ce012a6f7d1147c
DaveHyperspace <dave@hyperspace.us>
2023-03-08 12:49:09 +0000

    Chore: update index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
-import WordlePage from "./demo";
+import { GamesPlayed } from "@/ui/demo/game/GamesPlayed";
+import { Navbar } from "@/ui/demo/game/Navbar";
+import { MainContent } from "@/ui/demo/main-content";
+import { Footer } from "@/ui/footer";
+import { Layout } from "@/ui/layout";
 
 const HomePage = () => {
-  return <WordlePage />;
+  return (
+    <Layout>
+      <Navbar />
+      <GamesPlayed />
+      <MainContent />
+      <Footer />
+    </Layout>
+  );
 };
 
 export default HomePage;
