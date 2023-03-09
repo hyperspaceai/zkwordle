f8b82001f29cf5996556df95a083cfd570ca496d
DaveHyperspace <dave@hyperspace.us>
2023-03-09 19:41:08 +0000

    meta upate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@fontsource/jetbrains-mono/latin.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { Analytics } from "@vercel/analytics/react";
+import { metadata } from "config/metadata";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
@@ -14,6 +15,9 @@ const CustomApp = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
+        <meta content={`${metadata.url}/social.png`} property="og:image" />
+        <meta content={`${metadata.url}/social.png`} property="og:image:url" />
+        <meta content={`${metadata.url}/social.png`} property="og:image:secure_url" />
       </Head>
       <Analytics />
       <MetaTags />
