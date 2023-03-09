dc7b701e57a9600c3681d8999f4c997f4f90678e
DaveHyperspace <dave@hyperspace.us>
2023-03-09 20:11:37 +0000

    try meta data in docs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import "@fontsource/jetbrains-mono/latin.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { Analytics } from "@vercel/analytics/react";
-import { metadata } from "config/metadata";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
@@ -15,9 +14,6 @@ const CustomApp = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
-        <meta content={`${metadata.url}/social.png`} property="og:image" />
-        <meta content={`${metadata.url}/social.png`} property="og:image:url" />
-        <meta content={`${metadata.url}/social.png`} property="og:image:secure_url" />
       </Head>
       <Analytics />
       <MetaTags />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { FaviconTags } from "__generated__/favicon-tags";
 import { ColorModeScript } from "@chakra-ui/react";
+import { metadata } from "config/metadata";
 import { Head, Html, Main, NextScript } from "next/document";
 
 import { config } from "@/styles/config";
@@ -10,6 +11,9 @@ const Document = () => {
       <Head>
         <meta charSet="UTF-8" />
         <meta content="ie=edge" httpEquiv="X-UA-Compatible" />
+        <meta content={`${metadata.url}/social.png`} property="og:image" />
+        <meta content={`${metadata.url}/social.png`} property="og:image:url" />
+        <meta content={`${metadata.url}/social.png`} property="og:image:secure_url" />
         <FaviconTags />
       </Head>
       <body>
