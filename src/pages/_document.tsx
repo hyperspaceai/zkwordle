e82359c2829225495aea950584cfec9deeafd6b8
DaveHyperspace <dave@hyperspace.us>
2023-04-24 12:49:44 +0100

    Feat: add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,28 @@ import { Head, Html, Main, NextScript } from "next/document";
 import { config } from "@/styles/config";
 
 const Document = () => {
+  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
   return (
     <Html lang="en">
       <Head>
         <meta charSet="UTF-8" />
         <meta content="ie=edge" httpEquiv="X-UA-Compatible" />
         <FaviconTags />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_MEASUREMENT_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+          }}
+        />
       </Head>
       <body>
         <ColorModeScript initialColorMode={config.initialColorMode} />
